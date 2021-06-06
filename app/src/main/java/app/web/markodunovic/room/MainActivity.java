e79095ddd84eb2d4b1a35ce8fede9091096273a6
Marko Dunović <marko_dunovic@hotmail.com>
2021-06-06 09:01:51 +0200

    recycler added

--- app/src/main/java/app/web/markodunovic/room/MainActivity.java
+++ app/src/main/java/app/web/markodunovic/room/MainActivity.java
@@ -3,6 +3,8 @@ package app.web.markodunovic.room;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.widget.Toast;
@@ -17,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        final NoteAdapter adapter = new NoteAdapter();
+        recyclerView.setAdapter(adapter);
+
         noteViewModel = new ViewModelProvider(this).get(NoteViewModel.class);
         noteViewModel.getAllNotes().observe(this, new Observer<List<Note>>() {
             @Override
             public void onChanged(List<Note> notes) {
                 // update recycler view here
-                Toast.makeText(MainActivity.this, "On Changed", Toast.LENGTH_SHORT).show();
+               // Toast.makeText(MainActivity.this, "On Changed", Toast.LENGTH_SHORT).show();
+                adapter.setNotes(notes);
             }
         });
 
